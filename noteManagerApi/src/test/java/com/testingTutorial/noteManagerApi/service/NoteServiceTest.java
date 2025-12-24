97a8bca571ae189342498b82fdf8e1eb350becad
Pranjal Singh <155311442+06Pranjal@users.noreply.github.com>
2025-12-24 11:05:45 +0530

    Fixed dependencies and Junit 5 integrated

--- noteManagerApi/src/test/java/com/testingTutorial/noteManagerApi/service/NoteServiceTest.java
+++ noteManagerApi/src/test/java/com/testingTutorial/noteManagerApi/service/NoteServiceTest.java
@@ -1,46 +1,44 @@
 package com.testingTutorial.noteManagerApi.service;
 
 import com.testingTutorial.noteManagerApi.model.Note;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class NoteServiceTest {
 
-    private final NoteService noteService=new NoteService();
+    private final NoteService noteService = new NoteService();
 
-    @BeforeClass
-    public static void count(){
-        int c=0;
-        for(int i=1;i<10;i++){
-            c=c+i;
+    @BeforeAll
+     public static void count() {
+        int c = 0;
+        for (int i = 1; i < 10; i++) {
+            c = c + i;
         }
         System.out.println(c);
     }
+
     @Test
-    public void shouldCreateNoteSuccessfully(){
-        Note note=new Note(
+    @Disabled
+    void shouldCreateNoteSuccessfully() {
+        Note note = new Note(
                 1L,
                 "Test",
-                "Testing content");
-        Note saved=noteService.createNote(note);
+                "Testing content"
+        );
 
-        assertNotNull(saved);
-        assertEquals("Test",saved.getTitle());
-        assertEquals("Testing content",saved.getContent());
+        Note saved = noteService.createNote(note);
+
+        Assertions.assertNotNull(saved);
+        Assertions.assertEquals("Test", saved.getTitle());
+        Assertions.assertEquals("Testing content", saved.getContent(), "Fail");
+
         System.out.println(note);
-
     }
 
-    @AfterClass
-    public static void present(){
+    @AfterAll
+     public static void present() {
         System.out.println(new Date());
     }
-
 }
